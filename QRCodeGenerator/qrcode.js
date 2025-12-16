5174230ce7a02ded018f14aefb0ea2b91911301a
jodahen <jdhendriks@hotmail.com>
2025-12-16 15:12:26 +0100

    made more games

---
+++ QRCodeGenerator/qrcode.js
@@ -0,0 +1,13 @@
+let qrText = document.getElementById("textInput")
+let generateBtn = document.getElementById("generateBtn")
+
+let imgBox = document.getElementById("imgBox")
+let qrImage = document.getElementById("qrImage")
+
+function generateQR(text){
+    qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + encodeURIComponent(text);
+}
+
+generateBtn.addEventListener('click', () => {
+    generateQR(qrText.value)
+})
